624dfc53287bf8234f3d6c53d4979a10d2622793
GaneshLad2130 <ganeshlad2130@gmail.com>
2026-08-11 16:33:02 +0530

    Update products.js

--- data/products.js
+++ data/products.js
@@ -1,1 +1,50 @@
-
+const products = [
+    {
+        name: "मसाला शेव",
+        category: "नमकीन",
+        price: "₹___",
+        image: "images/masala-sev.jpg"
+    },
+    {
+        name: "घाटीया",
+        category: "नमकीन",
+        price: "₹___",
+        image: "images/ghatia.jpg"
+    },
+    {
+        name: "चिवडा",
+        category: "नमकीन",
+        price: "₹___",
+        image: "images/chivda.jpg"
+    },
+    {
+        name: "चाकली",
+        category: "नमकीन",
+        price: "₹___",
+        image: "images/chakali.jpg"
+    },
+    {
+        name: "मोतीचूर लाडू",
+        category: "मिठाई",
+        price: "₹___",
+        image: "images/motichoor-laddu.jpg"
+    },
+    {
+        name: "बेसन लाडू",
+        category: "मिठाई",
+        price: "₹___",
+        image: "images/besan-laddu.jpg"
+    },
+    {
+        name: "सोनपापडी",
+        category: "मिठाई",
+        price: "₹___",
+        image: "images/son-papdi.jpg"
+    },
+    {
+        name: "बालुशाही",
+        category: "मिठाई",
+        price: "₹___",
+        image: "images/balushahi.jpg"
+    }
+];
